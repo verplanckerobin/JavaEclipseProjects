dc24d3ec8ee78ae4f204d82411d49993086175f6
Robin Verplancke <verplanckerobin@gmail.com>
2020-12-04 11:24:20 +0100

    H5 Rekening update

--- H5/src/domein/Rekening.java
+++ H5/src/domein/Rekening.java
@@ -1,6 +1,6 @@
 package domein;
 
-public class Rekening {
+public class Rekening { //extends Object -> ook toString()-methode
 	
 	private long rekeningnummer;
 	private double saldo;
@@ -54,5 +54,30 @@ public class Rekening {
 		}
 		return false;
 	}
+	//sofie.schrijfBedragOverNaar(30, an);
+	public boolean schrijfBedragOverNaar(double bedrag, Rekening naarRek) { //zie UML
+		
+		//sofie = this -> praten met rek sofie
+		//an = naarRek -> praten met rek an
+		boolean gelukt = this.haalAf(bedrag);
+		if(gelukt) {
+			naarRek.stortOp(bedrag);
+			return true;
+		}
+		return false;
+	}
+	
+	//toString -> zorgt voor een Stringrepresentatie vh object, returned Object in stringformaat
+	//String.format(...);
+	//System.out.println(rek1.toString()); //expliciet
+	//System.out.println(rek1); //impliciete aanroep
+	@Override
+	public String toString() { //zie UML
+		
+		String res = String.format("Deze rekening heeft als houder %s, als rekeningNr %d en als saldo %.1f%n",
+				houder, rekeningnummer, saldo);
+		return res;
+	}
+	
 
 }
